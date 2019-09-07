0f43c97cdffcbd103cf3ef46f8e8fd22cf914c9a
Jonathan Traverso <jtraverso7@gmail.com>
2019-09-07 13:47:07 -0400

    Chef and recipe tables implemented.

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const server = require("./api/server");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}...`);
 });
